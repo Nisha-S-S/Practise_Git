aa4465f05d169dc33538b98be851d6de8dd7c0a1
Yash S S <yashss05@outlook.com>
2024-04-13 15:59:03 +0530

    Java file created

---
+++ mult_div.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class mult_div {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+		
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter the value of a");
+		a = sc.nextInt();
+		System.out.println("Enter the value of b");
+		b = sc.nextInt();
+		int add_ans = a+b;
+		System.out.println("The addition is : "+ add_ans);
+		
+		sc.close();
+		
+	}
+
+}
